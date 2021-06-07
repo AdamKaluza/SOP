29c8e37ab4304b7c7ca2a67dd1a6de233e082ae8
Adam <adam.kaluza.97@gmail.com>
2021-06-07 12:59:32 +0200

    zad 4,5

--- zadania/lab10/lab11/run.cpp
+++ zadania/lab10/lab11/run.cpp
@@ -6,28 +6,39 @@
 #include <iostream>
 #include <string.h>
 #include <signal.h>
-//zad1,2,3
+//zad1,2,3,4
+
+pid_t cpid;
+
+void handler(int signal){
+	kill(cpid,signal);
+}
 
 auto main (int argc, char *argv[]) -> int {
 	
-	auto cpid = fork();
+	cpid = fork();
 	
 	if (cpid == -1) {
 		perror("fork");
 		exit(EXIT_FAILURE);
 	}
 	if (cpid == 0) {
-		execve(argv[1], argv+1);
+		execvp(argv[1], argv+1);
 		perror("execve");
 		exit(EXIT_FAILURE);
 	}
 	
 	signal(SIGINT, SIG_IGN);
 	
+	struct sigaction act{};
+	act.sa_handler = handler;
+	
+	sigaction(SIGTERM, &act, nullptr); 
+	
 	int status = 0;
 	waitpid(cpid, &status, 0);
 	
-	if (WIFEXITED(status) {
+	if (WIFEXITED(status)) {
 		std::cout<<"Child PID is "<<cpid<<", status="<<WEXITSTATUS(status)<<"\n";
 	}
 	
@@ -36,6 +47,9 @@ auto main (int argc, char *argv[]) -> int {
 		WTERMSIG(status) << "\n" << strsignal(WTERMSIG(status)) << 
 		"\n";
 	}
+	
 		
 	return 0;
 }
+
+
